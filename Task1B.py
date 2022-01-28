4b6ce91af805dbeda42b8df87709295d2f089829
thomas-yam <cyy33@cam.ac.uk>
2022-01-28 22:56:10 +0000

    Testing

--- Task1B.py
+++ Task1B.py
@@ -1,1 +1,4 @@
-# Thomas
+from floodstation.geo import stations_by_distance
+
+# Task 1B
+print(stations_by_distance(1,(52.2053,0.1218)))
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,13 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+import haversine 
+
+def stations_by_distance(stations, p):
+    list = []
+    for station in stations:
+        distance = haversine(station, p)
+        list.append(tuple(station, distance))
+
+    return sorted_by_key(list, 1)

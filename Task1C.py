bc5ccea9605efd55f48529c8f9ceb7543feb449a
thomas-yam <cyy33@cam.ac.uk>
2022-01-29 00:27:53 +0000

    Task 1C completed

--- Task1C.py
+++ Task1C.py
@@ -1,6 +1,18 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
+def task1C(stations, centre, r):
+    lst = []
+    lst1 = stations_within_radius(stations, centre, r)
+    for i in lst1:
+        lst.append(i.name)
+    
+    print(sorted(lst))
+
+
+
+
 centre = (52.2053, 0.1218)
 r = 10
-print(stations_within_radius(build_station_list, centre, r))
+
+task1C(build_station_list(), centre, r)

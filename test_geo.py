3d32f18b8b5f5af8940ff5205cb39cd9c9af4480
thomas-yam <cyy33@cam.ac.uk>
2022-01-29 18:49:14 +0000

    Implemented pytest for geo functions

--- test_geo.py
+++ test_geo.py
@@ -6,28 +6,33 @@ from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 
 def test_stations_by_distance():
+    # Check output type
     stations = build_station_list()
     x = stations_by_distance(stations, (52.2053,0.1218))
 
     assert type(x) is list
 
 def test_stations_within_radius():
+    # Check output type
     stations = build_station_list()
     x = stations_within_radius(stations, (52.2053, 0.1218), 10) 
     
     assert type(x) is list 
 
+    # Check with one special case where the radius is 0
     y = stations_within_radius(stations, (52.2053, 0.1218), 0) 
 
     assert len(y) == 0
 
 def test_rivers_with_stations():
+    # Check output type
     stations = build_station_list()
     x = rivers_with_station(stations) 
 
     assert type(x) is list 
 
 def test_stations_by_river():
+    # Check output type
     stations = build_station_list()
     x = stations_by_river(stations) 
 

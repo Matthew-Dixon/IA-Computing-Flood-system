10dc30316371cca5aa07c1390bb0a8c7e6cfc269
Matthew-Dixon <matthewdixon.me@gmail.com>
2022-02-05 20:31:24 +0000

    reformatted output

--- Task1F.py
+++ Task1F.py
@@ -1,10 +1,11 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
+
 stations = build_station_list()
 inconsistent=inconsistent_typical_range_stations(stations)
 l=[]
 for s in inconsistent:
     l.append(s.name)
-print('Inconsistent Stations:',l)
+print('Task1f:/nInconsistent Stations:',l)
 
 

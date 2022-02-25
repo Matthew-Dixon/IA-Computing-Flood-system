c60493fd6f3aa1b4c0862411558542285822691f
Matthew-Dixon <matthewdixon.me@gmail.com>
2022-02-25 11:40:44 +0000

    implemented stations_highest_rel_level

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -18,3 +18,18 @@ def stations_level_over_threshold(stations, tol):
 
     return sorted_by_key(lst, 1, reverse=True)
 
+def stations_highest_rel_level(stations, N):
+
+    '''Returns a list of N stations with the highest relative
+    water level, sorted into descending order '''
+
+    list = []
+    for station in stations:
+        if station.relative_water_level() == None:
+            pass
+        else:
+            list.append((station, station.relative_water_level()))
+            
+    sortedlist = sorted_by_key(list, 1, reverse=True)
+    return [item[0] for item in sortedlist[0:N]]
+            

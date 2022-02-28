14a4a30348c4c738b02b23fd7cd833aa96ea5f4b
Matthew-Dixon <matthewdixon.me@gmail.com>
2022-02-28 14:59:27 +0000

    Adjusted tolereances, added colour to output

--- Task2G.py
+++ Task2G.py
@@ -1,22 +1,25 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.stationdata import update_water_levels
 from floodsystem.floodwarning import floodwarning
-
+from colorama import init
+init()
 def task2G():
     stations = build_station_list()
     update_water_levels(stations)
 
-    # Edit the toleracnce levels
-    tol_severe = 1
-    tol_high = 0.7
-    tol_moderate = 0.4
-    tol_low = 0.1
+    # towns with mean relative levels above these 
+    # thresholds are assigned to these categories
+    tol_severe = 2
+    tol_high = 1.6
+    tol_moderate = 1.3
+    tol_low = 1
 
     severe, high, moderate, low = floodwarning(stations, tol_severe, tol_high, tol_moderate, tol_low)
 
-    #print(f"Severe: {severe}")
-    #print(f"High: {high}")
-    #print(f"Moderate: {moderate}")
-    #print(f"Low: {low}")
+    print('\x1b[1;31;40m' + f"Severe: {severe}"+'\x1b[0m')
+    print('\x1b[1;33;40m' + f"High: {high}"+ '\x1b[0m')
+    print('\x1b[1;32;40m' +f"Moderate: {moderate}"+ '\x1b[0m')
+    print('\x1b[1;34;40m' +f"Low: {low}"+ '\x1b[0m')
+    
 
 task2G()
